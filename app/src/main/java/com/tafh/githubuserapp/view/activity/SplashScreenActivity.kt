aeb27d3cd2c40971617a483f9fbdecbed51fb984
tafhdytllah <taufikhh.97@gmail.com>
2022-09-13 21:14:55 +0700

    Create SplashScreenActivity

---
+++ app/src/main/java/com/tafh/githubuserapp/view/activity/SplashScreenActivity.kt
@@ -0,0 +1,27 @@
+package com.tafh.githubuserapp.view.activity
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.tafh.githubuserapp.R
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            callNextActivity()
+        }, 3000)
+    }
+
+    private fun callNextActivity() {
+        val intent = Intent(this@SplashScreenActivity, MainActivity:: class.java)
+        startActivity(intent)
+        finish()
+    }
+}
