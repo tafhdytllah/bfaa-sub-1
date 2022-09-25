d7d10bc92da1a2b7f62d3edef34e767d912265c7
tafhdytllah <taufikhh.97@gmail.com>
2022-09-25 16:09:27 +0700

    menambahkan library build.gradle:app dan build.gradle:module

--- app/src/main/java/com/tafh/githubuserapp/view/activity/MainActivity.kt
+++ app/src/main/java/com/tafh/githubuserapp/view/activity/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.tafh.githubuserapp.view.activity
 
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.tafh.githubuserapp.R
 import com.tafh.githubuserapp.databinding.ActivityMainBinding
